207528f654ce94b7c953f18ed2e13d30ade05eb0
tuta815 <arthur.imparato@hotmail.com>
2022-12-03 23:33:09 -0300

    primeiro commit MSTest/Xunit Test

---
+++ Projeto_de_testes/Projeto_de_testes/operacoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_de_testes
+{
+    public static class operacoes
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+    }
+}
---
+++ Projeto_de_testes/TestMSTest/TesteUnitario.cs
@@ -0,0 +1,45 @@
+﻿using Projeto_de_testes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMSTest
+{
+    [TestClass]
+    public class TesteUnitario
+    {
+        [TestMethod]
+        public void SomarDoisNumeros()
+        {
+            //Arrange - Preparacao
+            double pNum = 1;
+            double sNum = 1;
+            double rNum = 2;
+
+            //Act - Acao/Execucao
+            var Resultado = operacoes.Somar(pNum, sNum);
+
+            //Assert - Verificacao
+            Assert.AreEqual(rNum, Resultado);
+        }
+
+        //Arrange 2
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(2, 2, 4)]
+        [DataRow(2, 1, 3)]
+        [DataRow(3, 3, 6)]
+        [DataRow(4, 1, 5)]
+
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum)
+        {
+            //Act 2
+            var resultado = operacoes.Somar(pNum, sNum);
+
+            //Assert 2
+            Assert.AreEqual(rNum, resultado);
+        }       
+    }
+}
