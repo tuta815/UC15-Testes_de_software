d3b9bcca2036672beb63cd192d583a8ad7939991
tuta815 <arthur.imparato@hotmail.com>
2022-12-07 00:48:23 -0300

    commit encontro remoto 3

---
+++ Projeto_Teste/controller/LoginControllerTeste.cs
@@ -0,0 +1,38 @@
+﻿using Chapter.Controllers;
+using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Teste.controller
+{
+    public class LoginControllerTeste
+    {
+        [Fact]
+        public void LoginController_Retornar_Usuario_Invalido()
+        {
+            //Arrange
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+
+            LoginViewModel dadosLogin = new LoginViewModel();
+
+            dadosLogin.Email = "email@email.com";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //Act
+            var resultado = controller.Login(dadosLogin);
+
+            //Assert
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+        }
+    }
+}
